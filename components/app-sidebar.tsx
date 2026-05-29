e603cda8be419584268215bce00c8f3ce086dcad
andimags <andimagsumbol@gmail.com>
2026-05-29 17:00:58 +0800

    feat: update sidebar data, fix site header title to update depending on pathname

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,57 +14,8 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { HugeiconsIcon } from "@hugeicons/react"
-import {
-    DashboardSquare01Icon,
-    UserGroupIcon,
-    CommandIcon,
-    CableIcon,
-    ShieldUserIcon,
-    ShieldBlockchainIcon,
-    ShieldKeyIcon,
-} from "@hugeicons/core-free-icons"
-
-const data = {
-    user: {
-        name: "shadcn",
-        email: "m@example.com",
-        avatar: "/avatars/shadcn.jpg",
-    },
-    navMain: [
-        {
-            title: "Dashboard",
-            url: "/",
-            icon: (
-                <HugeiconsIcon icon={DashboardSquare01Icon} strokeWidth={2} />
-            ),
-        },
-        {
-            title: "Users",
-            url: "/users",
-            icon: <HugeiconsIcon icon={UserGroupIcon} strokeWidth={2} />,
-        },
-        {
-            title: "Channels",
-            url: "/channels",
-            icon: <HugeiconsIcon icon={CableIcon} strokeWidth={2} />,
-        },
-        {
-            title: "Roles",
-            url: "/roles",
-            icon: <HugeiconsIcon icon={ShieldUserIcon} strokeWidth={2} />,
-        },
-        {
-            title: "Policies",
-            url: "/policies",
-            icon: <HugeiconsIcon icon={ShieldBlockchainIcon} strokeWidth={2} />,
-        },
-        {
-            title: "Permissions",
-            url: "/permissions",
-            icon: <HugeiconsIcon icon={ShieldKeyIcon} strokeWidth={2} />,
-        }
-    ]
-}
+import { CommandIcon } from "@hugeicons/core-free-icons"
+import { sidebarData as data } from "@/constants/sidebarData"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
---
+++ constants/sidebarData.tsx
@@ -0,0 +1,53 @@
+import { HugeiconsIcon } from "@hugeicons/react"
+import {
+    DashboardSquare01Icon,
+    UserGroupIcon,
+    CableIcon,
+    ShieldUserIcon,
+    ShieldBlockchainIcon,
+    ShieldKeyIcon,
+} from "@hugeicons/core-free-icons"
+
+export const sidebarData = {
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+        {
+            title: "Dashboard",
+            url: "/",
+            icon: (
+                <HugeiconsIcon icon={DashboardSquare01Icon} strokeWidth={2} />
+            ),
+        },
+        {
+            title: "Users",
+            url: "/users",
+            icon: <HugeiconsIcon icon={UserGroupIcon} strokeWidth={2} />,
+        },
+        {
+            title: "Channels",
+            url: "/channels",
+            icon: <HugeiconsIcon icon={CableIcon} strokeWidth={2} />,
+        },
+        {
+            title: "Roles",
+            url: "/roles",
+            icon: <HugeiconsIcon icon={ShieldUserIcon} strokeWidth={2} />,
+        },
+        {
+            title: "Policies",
+            url: "/policies",
+            icon: <HugeiconsIcon icon={ShieldBlockchainIcon} strokeWidth={2} />,
+        },
+        {
+            title: "Permissions",
+            url: "/permissions",
+            icon: <HugeiconsIcon icon={ShieldKeyIcon} strokeWidth={2} />,
+        }
+    ]
+}
+
+export default sidebarData;
